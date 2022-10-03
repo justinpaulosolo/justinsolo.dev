fcacf868e86c00da746fdecbb5ff2eea550f2ca3
justinpaulosolo <justinpaulosolo@gmail.com>
2022-10-03 04:40:05 -0700

    chore: under construction page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,12 @@ import Container from "../components/Container";
 export default function Blog() {
   return (
     <Container title="Blog - Justin Solo">
-      <h1 className="text-6xl font-bold">Blog</h1>
+      <div className="flex flex-col w-full h-full">
+        <h1 className="text-6xl font-bold">Blog</h1>
+        <div className="mt-4 flex justify-center items-center">
+          <p className="text-2xl font-bold">✋ 🛑 Under construction</p>
+        </div>
+      </div>
     </Container>
   );
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,9 +3,10 @@ import Container from "../components/Container";
 export default function Projects() {
   return (
     <Container title="Projects - Justin Solo">
-      <div>
-        <div>
-          <h1 className="text-6xl tracking-tighter font-bold">Projects</h1>
+      <div className="flex flex-col w-full h-full">
+        <h1 className="text-6xl font-bold">Projects</h1>
+        <div className="mt-4 flex justify-center items-center">
+          <p className="text-2xl font-bold">✋ 🛑 Under construction</p>
         </div>
       </div>
     </Container>

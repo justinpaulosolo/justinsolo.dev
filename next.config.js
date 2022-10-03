60b7d422fdb7e71dc9429bc7362dd9c998d8167c
justinpaulosolo <justinpaulosolo@gmail.com>
2022-10-03 16:24:04 -0700

    feat: initial mdx blog commit

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
+module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], ...nextConfig })

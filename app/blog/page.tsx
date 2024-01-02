22be506f405fd6a436cb8cd355f9008b81cf65c2
Justin <JustinPauloSolo@gmail.com>
2024-01-02 12:29:40 -0800

    Update fonts and layout

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <section>
+      <h1>Blog Page</h1>
+    </section>
+  )
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectsPage() {
+  return (
+    <section>
+      <h1>Projects Page</h1>
+    </section>
+  )
+}

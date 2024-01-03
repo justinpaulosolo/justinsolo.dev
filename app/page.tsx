5294ab252f3d39132e7d0d65424692711204cfe5
Justin <JustinPauloSolo@gmail.com>
2024-01-03 00:39:39 -0800

    Add prettier-plugin-tailwindcss and update code formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className='mb-8 tracking-tighter font-bold text-4xl'>👋 Hello there.</h1>
-      <p>I&apos;m <strong>Justin</strong>, a full-stack developer passionate about creating scalable and efficient web applications</p>
-      <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
-        <a href="https://bun.sh">
-          BUN
-        </a>
+      <h1 className="mb-8 text-4xl font-bold tracking-tighter">
+        👋 Hello there.
+      </h1>
+      <p>
+        I&apos;m <strong>Justin</strong>, a full-stack developer passionate
+        about creating scalable and efficient web applications
+      </p>
+      <div className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800">
+        <a href="https://bun.sh">BUN</a>
       </div>
     </div>
-  )
+  );
 }

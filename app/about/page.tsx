9eda78787fec416e7a145bcaa7b861cf06d21c3d
Justin <JustinPauloSolo@gmail.com>
2024-01-03 14:24:18 -0800

    Add social media links to AboutPage component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,30 @@
+function SocialCard({ title, href }: { title: string; href: string }) {
+  return (
+    <div className="group flex w-full">
+      <a
+        target="_blank"
+        href={href}
+        className="flex w-full items-center justify-between text-nowrap rounded border border-neutral-200 p-1.5 font-semibold  dark:border-neutral-700 dark:text-neutral-100"
+      >
+        <p>{title}</p>
+        <div className="transform text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 dark:text-neutral-300">
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </a>
+    </div>
+  );
+}
 export default function AboutPage() {
   return (
     <section className="container space-y-6">
@@ -45,6 +72,20 @@ export default function AboutPage() {
             <h2 className="col-span-2 font-light tracking-wide text-neutral-700 dark:text-neutral-200">
               connect
             </h2>
+            <div className="col-span-4 flex justify-between gap-2">
+              <SocialCard
+                title="LinkedIn"
+                href="https://linkedin.com/justinpaulosolo"
+              />
+              <SocialCard
+                title="Github"
+                href="https://github.com/justinpaulosolo"
+              />
+              <SocialCard
+                title="Email"
+                href="mailto:justinpaulosolo@outlook.com"
+              />
+            </div>
           </div>
         </div>
       </div>

762a256556887fc2f1159e29752583c18600c3d6
Justin Solo <justinpaulosolo@gmail.com>
2024-08-06 08:06:16 -0700

    nav bar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "672px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
